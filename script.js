220b51624cd08acb4b2a584fc089de0ff1c4586b
ShadmanAhm3d <shadman01010@gmail.com>
2024-10-31 16:42:43 +0530

    Push

---
+++ script.js
@@ -0,0 +1,12 @@
+const btnAddtoChrome = document.querySelector("#HeroBtngreen");
+const HeroBtnWhite = document.querySelector("#HeroBtnWhite");
+
+
+btnAddtoChrome.addEventListener("click", () => {
+    // Set the href to the desired URL when the button is clicked
+    window.location.href = "https://github.com/ShadmanAhm3d/YoutubeShortsBlocker";
+});
+HeroBtnWhite.addEventListener("click", () => {
+    // Set the href to the desired URL when the button is clicked
+    window.location.href = "https://github.com/ShadmanAhm3d/YoutubeShortsBlocker";
+});
